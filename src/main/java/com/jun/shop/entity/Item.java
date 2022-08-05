e17ab084d8653dba30c4d9dd5e5df5a685684eda
junyoung <junyoungs7@naver.com>
2022-08-05 13:24:30 +0900

    init

--- src/main/java/com/jun/shop/entity/Item.java
+++ src/main/java/com/jun/shop/entity/Item.java
@@ -1,4 +1,36 @@
 package com.jun.shop.entity;
 
+import com.jun.shop.constant.ItemSellStatus;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "item")
+@Data
 public class Item {
+
+    @Id
+    @Column(name = "item_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String itemNm;
+
+    @Column(name = "price", nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int stockNumber;
+
+    @Lob
+    @Column(nullable = false)
+    private String itemDetail;
+
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus;
+    private LocalDateTime regTime;
+    private LocalDateTime updateTime;
 }
