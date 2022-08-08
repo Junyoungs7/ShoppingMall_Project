d959410969799e2fd6d02f10bba867988188ba99
junyoung <junyoungs7@naver.com>
2022-08-08 23:29:30 +0900

    init

---
+++ src/main/java/com/jun/shop/entity/ItemImg.java
@@ -0,0 +1,34 @@
+package com.jun.shop.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "item_img")
+@Data
+public class ItemImg extends BaseEntity {
+
+    @Id
+    @Column(name = "item_img_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String imgName;
+
+    private String oriImgName;
+
+    private String imgUrl;
+
+    private String repimgYn;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    public void updateItemImg(String oriImgName, String imgName, String imgUrl){
+        this.oriImgName = oriImgName;
+        this.imgName = imgName;
+        this.imgUrl = imgUrl;
+    }
+}
