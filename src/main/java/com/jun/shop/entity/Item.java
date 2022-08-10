5a11af7e53d3de59656dc5786c2c1240592806c3
junyoung <junyoungs7@naver.com>
2022-08-10 23:08:27 +0900

    init

--- src/main/java/com/jun/shop/entity/Item.java
+++ src/main/java/com/jun/shop/entity/Item.java
@@ -1,6 +1,7 @@
 package com.jun.shop.entity;
 
 import com.jun.shop.constant.ItemSellStatus;
+import com.jun.shop.dto.ItemFormDto;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -32,4 +33,11 @@ public class Item extends BaseEntity{
     @Enumerated(EnumType.STRING)
     private ItemSellStatus itemSellStatus;
 
+    public void updateItem(ItemFormDto itemFormDto){
+        this.itemNm = itemFormDto.getItemNm();
+        this.price = itemFormDto.getPrice();
+        this.stockNumber = itemFormDto.getStockNumber();
+        this.itemDetail = itemFormDto.getItemDetail();
+        this.itemSellStatus = itemFormDto.getItemSellStatus();
+    }
 }
